93ed600ed19ba40b93640a9b8198eef53fb96556
Janis Gailis <janis.gailis@uia.no>
2020-07-08 11:07:32 +0200

    Temporarly changed docker compose to not load schema.sql and added a first php file for a PHP course.

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <h2>Velkommen til Norge.no</h2>
+        <?php
+        echo "<p>Hovedstaden i Norge er: ";
+        echo "<strong>Oslo</strong>";
+        echo "<br />";
+        ?>
+
+        I dag er det den <?php echo date("d.m.Y");?>
+        </p>
+    </body>
+</html>
